c5e8a40e233bc2e65e638f69158bedd3d445ba92
NUdbrown <dbrown@student.neumont.edu>
2014-07-22 12:43:14 -0300

    added classes that need to be implemented

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,5 +1,33 @@
+import java.util.Iterator;
 
-public class LinkedList{
+import edu.neumont.util.Queue;
+
+
+public class LinkedList<T> implements Queue<T>{
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean offer(T t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 }
--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,12 +1,33 @@
+import java.util.Iterator;
 
-public class PriorityQueue {
+import edu.neumont.util.Queue;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+
+public class PriorityQueue<T> implements Queue<T>  {
+
+	@Override
+	public Iterator<T> iterator() {
 		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public T poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean offer(T t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
+
