838c375ee437343c363353d8f8b28242dd0ae953
NUdbrown <dbrown@student.neumont.edu>
2014-07-31 19:42:11 -0300

    Implemented Classes

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -4,36 +4,81 @@ import edu.neumont.util.List;
 
 
 public class ArrayList<T> implements List<T>{
+	
+	private T list [];  
+
+	private int currentNode;
+	
+	//getter and setter
+	
+	public ArrayList(){
+		T[] newList = (T[]) new Object[1];
+		newList[0] = null;
+		//set the list equal to the list to set it beginning position
+	}
 
 	@Override
 	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		setCurrentNode(0);
+		
+		
+		//start at the head
+		
+		return new ArraylistIterator(this);
 	}
 
 	@Override
 	public boolean add(T t) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean canAdd = false;
+		T[] newList = (T[]) new Object[list.length + 1];
+		//if there is nothing in the list add t to the first position in the list.
+		if(list[0] == null){
+			list[0] = t;
+			canAdd = true;
+		}
+		//iterate through the list and copy it to the new list.
+		else{
+			for(int i = 0; i < list.length-1; i++){
+				list[i] = newList[i];
+			}
+			
+		//if there is make new a list and copy everything and increase the size by 1
+			 newList[list.length +1]=t;
+			canAdd = true;
+		}
+		//set old list to new list
+		list = newList;
+		
+		return canAdd;
 	}
 
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return list[index];
 	}
 
 	@Override
 	public boolean remove(T t) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return list.length;
 	}
 
+	public int getCurrentNode() {
+		return currentNode;
+	}
+
+	public void setCurrentNode(int currentNode) {
+		this.currentNode = currentNode;
+	}
+
+	public T[] getList() {
+		return list;
+	}
 
 }
--- src/LinkedList.java
+++ src/LinkedList.java
@@ -4,29 +4,97 @@ import edu.neumont.util.Queue;
 
 
 public class LinkedList<T> implements Queue<T>{
+	
 
+	private Node<T> currentNode, head, tail;
+	
+
+	
+	public LinkedList(){
+		setCurrentNode(null);
+		head = null;
+		tail = null;
+	}
+
+	
 	@Override
 	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		setCurrentNode(head);
+		
+		return new LinkedListIterator<T>(this);
 	}
 
 	@Override
 	public T poll() {
-		// TODO Auto-generated method stub
-		return null;
+		T temp = null;
+		
+		if(head != null){
+			temp = head.getValue();
+			head = head.getAfterNode();
+			setCurrentNode(head);
+			return temp;
+		}
+		
+		return temp;
 	}
 
 	@Override
 	public boolean offer(T t) {
-		// TODO Auto-generated method stub
-		return false;
+
+		if(head == null){
+			head = new Node<T>(null, null, t);
+			tail = head;
+			
+		}
+		else{
+			Node<T> node = new Node<T>(tail, null,t);
+			tail.setAfterNode(node);
+			tail=node;
+			
+		}
+		
+		return true;
 	}
 
 	@Override
 	public T peek() {
-		// TODO Auto-generated method stub
+		
+		if(head != null){
+			return head.getValue();
+		}
 		return null;
+		
+		
+	}
+
+
+	public Node<T> getCurrentNode() {
+		return currentNode;
+	}
+
+
+	public void setCurrentNode(Node<T> currentNode) {
+		this.currentNode = currentNode;
+	}
+
+
+	public Node<T> getHead() {
+		return head;
+	}
+
+
+	public void setHead(Node<T> head) {
+		this.head = head;
+	}
+
+
+	public Node<T> getTail() {
+		return tail;
+	}
+
+
+	public void setTail(Node<T> tail) {
+		this.tail = tail;
 	}
 
 	
