808f08f3643a455be193919a4d35eaa2b900f9a0
NUdbrown <dbrown@student.neumont.edu>
2014-07-31 20:31:02 -0300

    Adding in the methods for Bank & Grocery

--- src/Bank.java
+++ src/Bank.java
@@ -1,30 +1,40 @@
 import edu.neumont.util.Client;
 import edu.neumont.util.QueueableService;
+import edu.neumont.util.Server;
 
 
 public class Bank implements QueueableService {
+	
+	private LinkedList<Client> numCustomers = new LinkedList<Client>();
+	private ArrayList<Server> numTellers = new ArrayList<Server>();
+	private int numberOfTellers;
+	
+	public Bank(int numberOfTellers){
+		this.numberOfTellers = numberOfTellers;
+		
+	}
 
 	@Override
 	public double getAverageClientWaitTime() {
-		// TODO Auto-generated method stub
+	
 		return 0;
 	}
 
 	@Override
 	public double getClientWaitTime(Client client) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public boolean addClient(Client client) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public void advanceMinute() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
--- src/GroceryStore.java
+++ src/GroceryStore.java
@@ -1,7 +1,34 @@
+import edu.neumont.util.Client;
+import edu.neumont.util.QueueableService;
 
-public class GroceryStore {
+
+public class GroceryStore implements QueueableService{
 	
-	public GroceryStore(){
+	public GroceryStore(int numberOfClerks){
+		
+	}
+
+	@Override
+	public double getAverageClientWaitTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getClientWaitTime(Client client) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean addClient(Client client) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void advanceMinute() {
+		// TODO Auto-generated method stub
 		
 	}
 
